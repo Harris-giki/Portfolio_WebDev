6bc4c8ce2ca1b8a78016bb20a035ae1ab4833d6c
Muhammad Haris <harris.giki@gmail.com>
2025-01-16 17:25:27 +0500

    new background

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener('mousemove', (e) => {
+    const { clientX, clientY } = e;
+    const gradients = document.querySelectorAll('.aurora-gradient');
+    
+    gradients.forEach((gradient, index) => {
+        const movement = (index + 1) * 2;
+        const x = (clientX - window.innerWidth / 2) / movement;
+        const y = (clientY - window.innerHeight / 2) / movement;
+        
+        gradient.style.transform = `translate(${x}px, ${y}px)`;
+    });
+});
